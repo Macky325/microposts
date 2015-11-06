1d1883b875734da69da933df91b96f13f7526d29
Tomoaki Makino <mochio47@gmail.com>
2015-11-06 05:15:37 +0000

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
     validates :name , length: { maximum: 20 } , presence: true
-    validates :place , length: { maximum: 20 } , presence: true
-    validates :profile , length: { minimum: 2 , maximum: 30 } , presence: true
+    validates :place , length: { maximum: 20 } , presence: true, on: :update
+    validates :profile , length: { minimum: 2 , maximum: 30 } , presence: true, on: :update
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
