baa0ba06d36a079557264409e027191d96d38b31
Tomoaki Makino <mochio47@gmail.com>
2015-11-09 08:27:36 +0000

    eee

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ActiveRecord::Base
+  validates :name , length: { maximum: 20 } , presence: true
+  validates :place , length: { maximum: 20 } , presence: true, on: :update
+  validates :profile , length: { minimum: 2 , maximum: 30 } , presence: true, on: :update
+  validates :email, presence: true, length: { maximum: 255 }, 
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+  has_many :microposts
+  has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
   has_secure_password
-    validates :name , length: { maximum: 20 } , presence: true
-    validates :place , length: { maximum: 20 } , presence: true, on: :update
-    validates :profile , length: { minimum: 2 , maximum: 30 } , presence: true, on: :update
-    validates :email, presence: true, uniqueness: { case_sensitive: false }
-    has_many :microposts
-    has_many :follower_relationships, class_name:  "Relationship",
-                                      foreign_key: "followed_id",
-                                      dependent:   :destroy
-    has_many :follower_users, through: :follower_relationships, source: :follower
-    
+   
   # 他のユーザーをフォローする
   def follow(other_user)
     following_relationships.create(followed_id: other_user.id)
