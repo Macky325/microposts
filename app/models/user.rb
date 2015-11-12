e9d6b5d1e00bab3a1e21158ffe870f76b7129dee
Tomoaki Makino <mochio47@gmail.com>
2015-11-12 08:21:13 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :name , length: { maximum: 20 } , presence: true
   validates :place , length: { maximum: 20 } , presence: true, on: :update
   validates :profile , length: { minimum: 2 , maximum: 30 } , presence: true, on: :update
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
@@ -10,6 +12,10 @@ class User < ActiveRecord::Base
                                     foreign_key: "followed_id",
                                     dependent:   :destroy
   has_many :follower_users, through: :follower_relationships, source: :follower
+  has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
   has_secure_password
    
   # 他のユーザーをフォローする
